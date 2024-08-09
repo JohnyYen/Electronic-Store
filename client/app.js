a8baa19759e32a7b312ba89e130ad1e1ef999857
Patty <patriciamieres3003@gmail.com>
2024-08-09 17:10:44 -0400

    more changes on the interface

--- client/app.js
+++ client/app.js
@@ -48,3 +48,15 @@ sliderContainer.addEventListener('touchend', function() {
         moveSlide(-1);
     }
 });
+
+let addPRoductWindow = document.getElementById("add-product-menu");
+let addProductBtn = document.getElementsByClassName("add-product");
+let addProductOption = document.getElementById("add-product");
+let selectPhotoBtn = document.getElementById("photo-input");
+let photoInput = document.getElementById("input");
+
+selectPhotoBtn.addEventListener('click', ()=>{
+    photoInput.click();
+});
+
+//HACER QUE APAREZCA UNA VISTA PREVIA DE LA IMAGEN SELECCIONADA
